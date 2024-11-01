a475360d4d4a38cc1fad4b99d73a5a1cc92548b4
Minh Duong <mduong1995@gmail.com>
2024-11-02 02:40:26 +0700

    feat: limit size payload and prevent only one payload onetime

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,10 +6,19 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
+	"net/http"
+	"strings"
 )
 
 func (app *application) readJSON(c *gin.Context, dest interface{}) error {
-	err := json.NewDecoder(c.Request.Body).Decode(dest)
+	// limit the size of request body to 1MB
+	var w = c.Writer
+	maxBytes := 1_048_576
+	c.Request.Body = http.MaxBytesReader(w, c.Request.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(c.Request.Body)
+	dec.DisallowUnknownFields() // disallow unknown fields in payload
+	err := dec.Decode(dest)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -26,11 +35,22 @@ func (app *application) readJSON(c *gin.Context, dest interface{}) error {
 			return fmt.Errorf("body contains incorrect JSON type for %q", unmarshalTypeError.Value)
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
 		default:
 			return err
 		}
 	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
 	return nil
 }
