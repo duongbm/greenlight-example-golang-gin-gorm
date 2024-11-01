65ea3fb7459112e7422fd84d4e6656c03319caa2
Minh Duong <mduong1995@gmail.com>
2024-11-02 01:52:46 +0700

    feat: update routes NoRoute & NoMethod

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,6 +5,9 @@ import "github.com/gin-gonic/gin"
 func (app *application) routes() *gin.Engine {
 	router := gin.Default()
 
+	router.NoRoute(app.notFoundResponse)
+	router.NoMethod(app.methodNotAllowedResponse)
+
 	// health check handler
 	router.GET("/health", app.healthcheckHandler)
 
