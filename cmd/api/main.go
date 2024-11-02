d3c03c35d6c24e4e2476ad136af4417d230a1ab2
Minh Duong <mduong1995@gmail.com>
2024-11-03 03:42:09 +0700

    feat: add connection db to main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
 	"os"
@@ -17,6 +20,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn         string
+		maxConn     int
+		MaxIdleConn int
+		maxIdleTime string
+	}
 }
 
 // define an application struct to hold dependencies for HTTP handler, helper, middlewares, ...
@@ -31,10 +40,22 @@ func main() {
 	// Read value of port and env command-line flags into config struct
 	flag.IntVar(&cfg.port, "port", 8000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "environment(development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DB_DSN"), "PostgreSQL connection DSN")
+	flag.IntVar(&cfg.db.maxConn, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.MaxIdleConn, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max idle timeout")
 	flag.Parse()
 
 	// Initialize a new logger which write messages to the standard out stream
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// call openDB() to create then connection pool
+	_, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	logger.Printf("database connection pool established.")
 
 	// Declare an instance of application struct, containing the config struct and logger
 	app := &application{
@@ -52,6 +73,37 @@ func main() {
 
 	// Start HTTP Server
 	logger.Printf("Starting %s server on %d", cfg.env, cfg.port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(cfg.db.dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	// create a context with 5 second timeout deadline
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxOpenConns(cfg.db.maxConn)
+	sqlDB.SetMaxIdleConns(cfg.db.MaxIdleConn)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	sqlDB.SetConnMaxIdleTime(duration)
+
+	err = sqlDB.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
